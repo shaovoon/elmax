aac441b3febd6fbf890fbee3c3fef71b3f2fc23d
shaovoon <shaovoon@yahoo.com>
2024-04-27 13:58:47 +0800

    Fix the linker error of Elmax and SequelMax

--- PortableElmax/PortableElmax/Elmax.h
+++ PortableElmax/PortableElmax/Elmax.h
@@ -6,52 +6,112 @@
 
 #pragma once
 #if _MSC_VER == 1400
-#	ifdef _DEBUG
-#		pragma comment(lib, "../Debug/PortableElmax_VS2005D.lib")
-#		pragma comment(lib, "../Debug/FileIO_VS2005D.lib")
-#	else 
-#		pragma comment(lib, "../Release/PortableElmax_VS2005.lib")
-#		pragma comment(lib, "../Release/FileIO_VS2005.lib")
+#   if defined(_WIN64)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../x64/Debug/PortableElmax_VS2005D.lib")
+#		    pragma comment(lib, "../x64/Debug/FileIO_VS2005D.lib")
+#	    else 
+#		    pragma comment(lib, "../x64/Release/PortableElmax_VS2005.lib")
+#		    pragma comment(lib, "../x64/Release/FileIO_VS2005.lib")
+#       endif
+#   elif defined(_WIN32)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../Debug/PortableElmax_VS2005D.lib")
+#		    pragma comment(lib, "../Debug/FileIO_VS2005D.lib")
+#	    else 
+#		    pragma comment(lib, "../Release/PortableElmax_VS2005.lib")
+#		    pragma comment(lib, "../Release/FileIO_VS2005.lib")
+#       endif
 #	endif
 #elif _MSC_VER == 1500
-#	ifdef _DEBUG
-#		pragma comment(lib, "../Debug/PortableElmax_VS2008D.lib")
-#		pragma comment(lib, "../Debug/FileIO_VS2008D.lib")
-#	else 
-#		pragma comment(lib, "../Release/PortableElmax_VS2008.lib")
-#		pragma comment(lib, "../Release/FileIO_VS2008.lib")
+#   if defined(_WIN64)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../x64/Debug/PortableElmax_VS2008D.lib")
+#		    pragma comment(lib, "../x64/Debug/FileIO_VS2008D.lib")
+#	    else 
+#		    pragma comment(lib, "../x64/Release/PortableElmax_VS2008.lib")
+#		    pragma comment(lib, "../x64/Release/FileIO_VS2008.lib")
+#       endif
+#   elif defined(_WIN32)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../Debug/PortableElmax_VS2008D.lib")
+#		    pragma comment(lib, "../Debug/FileIO_VS2008D.lib")
+#	    else 
+#		    pragma comment(lib, "../Release/PortableElmax_VS2008.lib")
+#		    pragma comment(lib, "../Release/FileIO_VS2008.lib")
+#       endif
 #	endif
 #elif _MSC_VER == 1600
-#	ifdef _DEBUG
-#		pragma comment(lib, "../Debug/PortableElmax_VS2010D.lib")
-#		pragma comment(lib, "../Debug/FileIO_VS2010D.lib")
-#	else 
-#		pragma comment(lib, "../Release/PortableElmax_VS2010.lib")
-#		pragma comment(lib, "../Release/FileIO_VS2010.lib")
+#   if defined(_WIN64)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../x64/Debug/PortableElmax_VS2010D.lib")
+#		    pragma comment(lib, "../x64/Debug/FileIO_VS2010D.lib")
+#	    else 
+#		    pragma comment(lib, "../x64/Release/PortableElmax_VS2010.lib")
+#		    pragma comment(lib, "../x64/Release/FileIO_VS2010.lib")
+#       endif
+#   elif defined(_WIN32)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../Debug/PortableElmax_VS2010D.lib")
+#		    pragma comment(lib, "../Debug/FileIO_VS2010D.lib")
+#	    else 
+#		    pragma comment(lib, "../Release/PortableElmax_VS2010.lib")
+#		    pragma comment(lib, "../Release/FileIO_VS2010.lib")
+#       endif
 #	endif
 #elif _MSC_VER == 1700
-#	ifdef _DEBUG
-#		pragma comment(lib, "../Debug/PortableElmaxD.lib")
-#		pragma comment(lib, "../Debug/FileIO.lib")
-#	else 
-#		pragma comment(lib, "../Release/PortableElmax.lib")
-#		pragma comment(lib, "../Release/FileIO.lib")
+#   if defined(_WIN64)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../x64/Debug/PortableElmaxD.lib")
+#		    pragma comment(lib, "../x64/Debug/FileIO.lib")
+#	    else 
+#		    pragma comment(lib, "../x64/Release/PortableElmax.lib")
+#		    pragma comment(lib, "../x64/Release/FileIO.lib")
+#       endif
+#   elif defined(_WIN32)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../Debug/PortableElmaxD.lib")
+#		    pragma comment(lib, "../Debug/FileIO.lib")
+#	    else 
+#		    pragma comment(lib, "../Release/PortableElmax.lib")
+#		    pragma comment(lib, "../Release/FileIO.lib")
+#       endif
 #   endif
 #elif _MSC_VER == 1800
-#	ifdef _DEBUG
-#		pragma comment(lib, "../Debug/PortableElmax_VS2013D.lib")
-#		pragma comment(lib, "../Debug/FileIO_VS2013D.lib")
-#	else 
-#		pragma comment(lib, "../Release/PortableElmax_VS2013.lib")
-#		pragma comment(lib, "../Release/FileIO_VS2013.lib")
-#	endif
+#   if defined(_WIN64)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../x64/Debug/PortableElmax_VS2013D.lib")
+#		    pragma comment(lib, "../x64/Debug/FileIO_VS2013D.lib")
+#	    else 
+#		    pragma comment(lib, "../x64/Release/PortableElmax_VS2013.lib")
+#		    pragma comment(lib, "../x64/Release/FileIO_VS2013.lib")
+#	    endif
+#   elif defined(_WIN32)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../Debug/PortableElmax_VS2013D.lib")
+#		    pragma comment(lib, "../Debug/FileIO_VS2013D.lib")
+#	    else 
+#		    pragma comment(lib, "../Release/PortableElmax_VS2013.lib")
+#		    pragma comment(lib, "../Release/FileIO_VS2013.lib")
+#	    endif
+#    endif
 #else
-#	ifdef _DEBUG
-#		pragma comment(lib, "../Debug/PortableElmaxD.lib")
-#		pragma comment(lib, "../Debug/FileIO.lib")
-#	else 
-#		pragma comment(lib, "../Release/PortableElmax.lib")
-#		pragma comment(lib, "../Release/FileIO.lib")
+#   if defined(_WIN64)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../x64/Debug/PortableElmaxD.lib")
+#		    pragma comment(lib, "../x64/Debug/FileIO.lib")
+#	    else 
+#		    pragma comment(lib, "../x64/Release/PortableElmax.lib")
+#		    pragma comment(lib, "../x64/Release/FileIO.lib")
+#       endif
+#   elif defined(_WIN32)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../Debug/PortableElmaxD.lib")
+#		    pragma comment(lib, "../Debug/FileIO.lib")
+#	    else 
+#		    pragma comment(lib, "../Release/PortableElmax.lib")
+#		    pragma comment(lib, "../Release/FileIO.lib")
+#       endif
 #   endif
 #endif
 
--- PortableElmax/SequelMax/SequelMax.h
+++ PortableElmax/SequelMax/SequelMax.h
@@ -7,28 +7,76 @@
 #pragma once
 
 #if _MSC_VER == 1600
-#	ifdef _DEBUG
-#		pragma comment(lib, "../Debug/SequelMax_VS2010D.lib")
-#		pragma comment(lib, "../Debug/FileIO_VS2010D.lib")
-#	else 
-#		pragma comment(lib, "../Release/SequelMax_VS2010.lib")
-#		pragma comment(lib, "../Release/FileIO_VS2010.lib")
+#   if defined(_WIN64)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../x64/Debug/SequelMax_VS2010D.lib")
+#		    pragma comment(lib, "../x64/Debug/FileIO_VS2010D.lib")
+#	    else 
+#		    pragma comment(lib, "../x64/Release/SequelMax_VS2010.lib")
+#		    pragma comment(lib, "../x64/Release/FileIO_VS2010.lib")
+#	    endif
+#   elif defined(_WIN32)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../Debug/SequelMax_VS2010D.lib")
+#		    pragma comment(lib, "../Debug/FileIO_VS2010D.lib")
+#	    else 
+#		    pragma comment(lib, "../Release/SequelMax_VS2010.lib")
+#		    pragma comment(lib, "../Release/FileIO_VS2010.lib")
+#	    endif
 #	endif
 #elif _MSC_VER == 1700
-#	ifdef _DEBUG
-#		pragma comment(lib, "../Debug/SequelMax_VS2012D.lib")
-#		pragma comment(lib, "../Debug/FileIO.lib")
-#	else 
-#		pragma comment(lib, "../Release/SequelMax_VS2012.lib")
-#		pragma comment(lib, "../Release/FileIO.lib")
-#   endif
+#   if defined(_WIN64)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../x64/Debug/SequelMax_VS2012D.lib")
+#		    pragma comment(lib, "../x64/Debug/FileIO.lib")
+#	    else 
+#		    pragma comment(lib, "../x64/Release/SequelMax_VS2012.lib")
+#		    pragma comment(lib, "../x64/Release/FileIO.lib")
+#       endif
+#   elif defined(_WIN32)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../Debug/SequelMax_VS2012D.lib")
+#		    pragma comment(lib, "../Debug/FileIO.lib")
+#	    else 
+#		    pragma comment(lib, "../Release/SequelMax_VS2012.lib")
+#		    pragma comment(lib, "../Release/FileIO.lib")
+#       endif
+#	endif
 #elif _MSC_VER == 1800
-#	ifdef _DEBUG
-#		pragma comment(lib, "../Debug/SequelMax_VS2013D.lib")
-#		pragma comment(lib, "../Debug/FileIO_VS2013D.lib")
-#	else 
-#		pragma comment(lib, "../Release/SequelMax_VS2013.lib")
-#		pragma comment(lib, "../Release/FileIO_VS2013.lib")
+#   if defined(_WIN64)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../x64/Debug/SequelMax_VS2013D.lib")
+#		    pragma comment(lib, "../x64/Debug/FileIO_VS2013D.lib")
+#	    else 
+#		    pragma comment(lib, "../x64/Release/SequelMax_VS2013.lib")
+#		    pragma comment(lib, "../x64/Release/FileIO_VS2013.lib")
+#	    endif
+#   elif defined(_WIN32)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../Debug/SequelMax_VS2013D.lib")
+#		    pragma comment(lib, "../Debug/FileIO_VS2013D.lib")
+#	    else 
+#		    pragma comment(lib, "../Release/SequelMax_VS2013.lib")
+#		    pragma comment(lib, "../Release/FileIO_VS2013.lib")
+#	    endif
+#	endif
+#else
+#   if defined(_WIN64)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../x64/Debug/SequelMaxD.lib")
+#		    pragma comment(lib, "../x64/Debug/FileIO.lib")
+#	    else 
+#		    pragma comment(lib, "../x64/Release/SequelMax.lib")
+#		    pragma comment(lib, "../x64/Release/FileIO.lib")
+#	    endif
+#   elif defined(_WIN32)
+#	    ifdef _DEBUG
+#		    pragma comment(lib, "../Debug/SequelMaxD.lib")
+#		    pragma comment(lib, "../Debug/FileIOD.lib")
+#	    else 
+#		    pragma comment(lib, "../Release/SequelMax.lib")
+#		    pragma comment(lib, "../Release/FileIO.lib")
+#	    endif
 #	endif
 #endif
 
